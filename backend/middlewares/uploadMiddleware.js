50b1491eedfb9db01e9c28d1d4fae5ac26370860
VarunBathmi <varunbhoi7763@gmail.com>
2026-05-02 19:27:56 +0530

    resolve bug  in Middleware

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,20 +1,31 @@
-const multer = require('multer');
+const multer = require("multer");
 
 //Configure storage
 const storage = multer.diskStorage({
-    destination: (req, filr, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
 //File Filter
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/jpeg','image/png','image/jpg','application/pdf'];
-    if(allowedTypes.includes(file.minetype)){
-        cb(null, true);
-    } else {
-        cb(new Error('Only .jpeg, .jpg, .png, and .pdf formats are allowed'), false);
-    }
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/jpg",
+    "application/pdf",
+  ];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error("Only .jpeg, .jpg, .png, and .pdf formats are allowed"),
+      false,
+    );
+  }
 };
 
 const upload = multer({ storage, fileFilter });
