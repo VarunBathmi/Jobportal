cce8c95d3356c0e4674731701d9b285561d8b1b5
VarunBathmi <varunbhoi7763@gmail.com>
2026-05-03 20:14:28 +0530

    adding src component folder

---
+++ frontend/src/pages/JobSeeker/components/FilterContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FilterContent = ({
+  toggleSection,
+  clearAllfilters,
+  expandedSections,
+  filters,
+  handleFilterChange,
+}) => {
+  return <div>FilterContent</div>;
+};
+
+export default FilterContent;
