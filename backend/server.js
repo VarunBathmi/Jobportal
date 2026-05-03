f42a4b695734c0ffa6b2a579ede2c5bf09dbdc42
Yash solanki <yashj5155@gmail.com>
2026-05-03 18:15:43 +0530

    Building Saved Job APIs

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const userRoutes = require("./routes/userRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 const savedJobsRoutes = require("./routes/savedJobsRoutes");
+const analyticsRoutes = require("./routes/analyticsRoutes");
+
 
 const app = express();
 
@@ -33,8 +35,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 // JobRoutes
 app.use("/api/jobs", jobRoutes);
-app.use("/api/applications",applicationRouters);
+app.use("/api/applications", applicationRoutes);
 app.use("/api/save-jobs", savedJobsRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
 
 //Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
